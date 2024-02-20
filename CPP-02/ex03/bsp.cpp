975039070ad6c23e3e3c3bdd644b1ceff6d71b85
Oriol Jimenez Garrich <ojimenez@car4s3.42barcelona.com>
2024-02-20 13:37:32 +0100

    CPP-02 terminado

--- CPP-02/ex03/bsp.cpp
+++ CPP-02/ex03/bsp.cpp
@@ -6,11 +6,12 @@
 /*   By: ojimenez <ojimenez@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 16:24:49 by ojimenez          #+#    #+#             */
-/*   Updated: 2024/02/19 16:24:49 by ojimenez         ###   ########.fr       */
+/*   Updated: 2024/02/20 13:33:50 by ojimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
+#include <iostream>
 
 int	orientacionTriangulo(Point const &a, Point const &b, Point const &c)
 {
@@ -22,7 +23,6 @@ int	orientacionTriangulo(Point const &a, Point const &b, Point const &c)
 	/*normal = ((b.getX() - a.getX()) * (c.getY() - a.getY()))
 			- ((b.getY() - a.getY()) * (c.getX() - a.getX()));*/
 
-
 	if (normal.toFloat() > 0.0)
 		return (1);
 	else if (normal.toFloat() < 0.0)
@@ -33,20 +33,17 @@ int	orientacionTriangulo(Point const &a, Point const &b, Point const &c)
 
 bool bsp( Point const &a, Point const &b, Point const &c, Point const &point)
 {
-	Point	ab;
-	Point	ac;
-	bool	oTriangulo;
 	bool	in;
 
 	if (point == a || point == b || point == c)
 		return (false);
 
-	oTriangulo = orientacionTriangulo(a, b, c);
-	bool orientacion1 = orientacionTriangulo(point, a, b);
-	bool orientacion2 = orientacionTriangulo(point, b, c);
-	bool orientacion3 = orientacionTriangulo(point, c, a);
+	//nt oTriangulo = orientacionTriangulo(a, b, c);
+	int orientacion1 = orientacionTriangulo(point, a, b);
+	int orientacion2 = orientacionTriangulo(point, b, c);
+	int orientacion3 = orientacionTriangulo(point, c, a);
 
-	if ((orientacion1 && orientacion2 && orientacion3) || (!orientacion1 && !orientacion2 && !orientacion3))
+	if (orientacion1 == orientacion2 && orientacion2 == orientacion3 && orientacion1 != 0)
 		in = true;
 	else
 		in = false;
--- CPP-02/ex03/main.cpp
+++ CPP-02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ojimenez <ojimenez@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 16:23:43 by ojimenez          #+#    #+#             */
-/*   Updated: 2024/02/19 16:23:43 by ojimenez         ###   ########.fr       */
+/*   Updated: 2024/02/20 13:34:18 by ojimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ int main(void)
 	Point const	p2(0.0f, 2.0f);
 	Point const	p3(5.0f, 0.0f);
 
-	//Point	point(5.0f, 10.5f);
-	//Point point(2.0f, 1.0f);
-	//Point point(1.0, 1.0);
-	//Point point(p3);
-	Point point(0.0f, 1.0f);
+	Point	point(5.0f, 10.5f); //fuera
+	//Point point(2.0f, 1.0f); //Dentro
+	//Point point(1.0, 1.0); //Dentro
+	//Point point(p3); //En una punta
+	//Point point(0.0f, 1.0f); //en la linea del contorno
 
 
 	if (bsp(p1, p2, p3, point))
