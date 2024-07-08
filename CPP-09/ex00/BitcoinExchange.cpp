356c1bd5c09150fae6c3f9ae2b91b6909d489af5
Oriol Jiménez Garrich <ojimenez@car2s5.42barcelona.com>
2024-07-08 20:09:42 +0200

    Actualitzat

--- CPP-09/ex00/BitcoinExchange.cpp
+++ CPP-09/ex00/BitcoinExchange.cpp
@@ -3,10 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ojimenez <ojimenez@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: ojimenez <ojimenez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 18:18:10 by ojimenez          #+#    #+#             */
-/*   Updated: 2024/07/01 18:18:10 by ojimenez         ###   ########.fr       */
+/*   Updated: 2024/07/08 18:13:30 by ojimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange()
+{
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &cpy)
+{
+    *this = cpy;
+}
+
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &cpy)
+{
+    if (this != &cpy)
+    {
+        this->data = cpy.data;
+    }
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
--- CPP-09/ex00/BitcoinExchange.hpp
+++ CPP-09/ex00/BitcoinExchange.hpp
@@ -3,10 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ojimenez <ojimenez@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: ojimenez <ojimenez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 18:18:18 by ojimenez          #+#    #+#             */
-/*   Updated: 2024/07/01 18:18:18 by ojimenez         ###   ########.fr       */
+/*   Updated: 2024/07/08 17:41:50 by ojimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
+
+# include <iostream>
+# include "map"
+
+class BitcoinExchange
+{
+	private:
+		std::map<std::string, float> data;
+	
+	public:
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange &cpy);
+		BitcoinExchange &operator=(const BitcoinExchange &cpy);
+		~BitcoinExchange();
+};
+
 
